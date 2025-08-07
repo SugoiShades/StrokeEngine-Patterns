c8988a9f935a4792217a2e45cd22319665147489
SugoiShades <github@serket.gay>
2025-08-06 22:21:44 -0500

    Updated Knot pattern with changes made by other communicty members!

--- Patterns-By-Serket/Pattern (Knot) V2.h
+++ Patterns-By-Serket/Pattern (Knot) V2.h
@@ -3,6 +3,10 @@
   @brief  Knot: a modification of my Struggle pattern to have a pause added
   at the end of the stroke and to use sensation to change the speed of the
   slow portion of the stroke rather than how much of the stroke is slow.
+
+  V1 Initial pattern made by Serket
+
+  V2 tweaks made by Vampix (work in progress)
 */
 /**************************************************************************/
 
@@ -14,7 +18,7 @@ class Knot : public Pattern {
                 _speed = speed;
             }
             void setSensation(float sensation) {
-                _sensation = float(abs((sensation) / 50) + 0.1);
+                _sensation = float(abs((sensation) / 1000) + 0.001);
             }
             void _updateDelay(int delayInMillis) {
                 _delayInMillis = delayInMillis;
@@ -25,27 +29,32 @@ class Knot : public Pattern {
                 //fancy equation to calculate the pause in milliseconds using the current speed as an input. put it into the Desmos web graphing calculator
                 //with Y as the delay in ms and X as speed to get an idea for a given speed what the corrosponding delay would be. Tweak the 3 numbers in Desmos
                 //and not here in the pattern as it's hard to conceptualize how it'll change.
-                _delayInMillis = ((sqrt((350000 * _speed)+60000)) + 125);
+                _delayInMillis = ((sqrt((350000 * _speed)+60000)) + 550);
 
                 if (_isStillDelayed() == false) {
-                    if (index % 3 == 1) {
-                        _nextMove.acceleration = int(3 * _nextMove.speed/_timeOfStroke);
-                        _nextMove.speed = int(1.5 * _stroke/_timeOfStroke);
+                    if (index % 5 == 1) {
+                        _nextMove.acceleration = int(2 * _nextMove.speed/_timeOfStroke);
+                        _nextMove.speed = int(0.8 * _stroke/_timeOfStroke);
                         //70% of the stroke looks good visually. tweak the below 0.70 to change the % of the in stroke is at normal speed
                         _nextMove.stroke = int((_depth - _stroke) + (_stroke * 0.70));
                     }
-                    else if (index % 3 == 2) {
+                    else if (index % 5 == 2) {
+                        _startDelay();
+                    }
+                    else if (index % 5 == 3) {
                         _nextMove.acceleration = int(2.3 * _nextMove.speed/_timeOfStroke);
                         _nextMove.speed = int(_sensation * _stroke/_timeOfStroke);
                         _nextMove.stroke = _depth;
                         //Uncomment the following 2 lines to see the current speed value printed and then the corrosponding delay in ms. Useful for tweaking the equation for tweaking the delay curve.
-                        //Serial.println("Speed: " + String(_speed));
-                        //Serial.println("Delay in ms: " + String(_delayInMillis));
+                        Serial.println("Speed: " + String(_speed));
+                        Serial.println("Delay in ms: " + String(_delayInMillis));
+                    }
+                    else if (index % 5 == 4) {
                         _startDelay();
                     }
                     else  {
-                        _nextMove.acceleration = int(2.9 * _nextMove.speed/_timeOfStroke);
-                        _nextMove.speed = int(1.5 * _stroke/_timeOfStroke);
+                        _nextMove.acceleration = int(2 * _nextMove.speed/_timeOfStroke);
+                        _nextMove.speed = int(1.0 * _stroke/_timeOfStroke);
                         _nextMove.stroke = _depth - _stroke;
                     }
                     _nextMove.skip = false;
